5089d66701313aad350948fa26c0de5585af3300
ddnosh <cage.hung@gmail.com>
2019-08-13 15:21:15 +0800

    增加实例

--- tinyui-sample/src/main/java/com/androidwind/ui/sample/MainActivity.java
+++ tinyui-sample/src/main/java/com/androidwind/ui/sample/MainActivity.java
@@ -2,6 +2,10 @@ package com.androidwind.ui.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.androidwind.ui.handler.TinyTextViewHandler;
+import com.androidwind.ui.view.TinyTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void BtnClick(View view) {
+        TinyTextView ttv = findViewById(R.id.ttv);
+        //获取Helper
+        TinyTextViewHandler handler = ttv.getViewHandler();
+        handler.setBackgroundColorNormal(getResources().getColor(android.R.color.holo_red_dark))
+                .setBorderColorNormal(getResources().getColor(android.R.color.holo_orange_light))
+                .setBorderWidthNormal(12)
+                .setCornerRadius(25);
     }
 }
